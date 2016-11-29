c42b7f7b6ad483ccb02e4cc2c2a10bc47d441471
Lee O'Keefe <okeefe.lee@gmail.com>
2016-11-29 15:52:13 -0800

    update old password

--- lib/devise_security_extension/models/mongoid/old_password.rb
+++ lib/devise_security_extension/models/mongoid/old_password.rb
@@ -4,11 +4,6 @@ class OldPassword
 
   field :encrypted_password, type: String
   field :password_salt, type: String
-  field :password_archivable_id, type: Integer
-  field :password_archivable_type, type: String
-  
-  field :created_at, type: Time
 
   belongs_to :password_archivable, polymorphic: true
-  attr_accessible :encrypted_password
 end 
